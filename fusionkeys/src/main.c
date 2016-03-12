b10967b741a763be99f062e8dce5862bd25340a9
Marlox <fusionkeys@mx.ouda.fr>
2016-03-12 04:16:01 +0100

    [fk] Update of POC with hardcoded conversion

--- fusionkeys/src/main.c
+++ fusionkeys/src/main.c
@@ -6,8 +6,15 @@
 int main(void) {
     log_info("Fusion Keys POC starts\n");
     char buf[256];
+    fk_init();
+    map_put('a', 98);
+    map_put('b', 100);
+    map_put('c', 97);
+    map_put('d', 120);
+    map_put('e', 140);
     fk_read(buf, 256);
     printf("Readed keys: %s", buf);
+    fk_exit();
     log_info("Fusion Keys POC ends\n");
     return 0;
 }
